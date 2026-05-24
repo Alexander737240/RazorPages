e52c801898cd2374066cb3792cbdefd3533ec6a2
alex <kond9.k@yandex.ru>
2026-05-24 12:33:29 +0300

    Models DONE

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -6,8 +7,17 @@ namespace ContosoUniversity.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)] 
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0,5)]
         public int Credits { get; set; }
+
+        public int DepartmentID { get; set; } //Внешний ключ
+
+        public Department Department { get; set; }
+        public ICollection<Instructor> Instructors { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Без фамилии никак")]
+        [StringLength(50, ErrorMessage = "Первышено максимальное количество символов")]
+        [RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Введены недопустимые символы")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Без имени никак")]
+        [StringLength(50, ErrorMessage = "Первышено максимальное количество символов")]
+        [RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Введены недопустимые символы")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Дата трудоустройства")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name ="Преподаватель")]
+        public string FullName { get => $"{LastName}{FirstName}";}
+
+        public OfficeAssignment OfficeAssignment { get; set; }
+        public ICollection<Course> Courses { get; set; }
+
+    }
+}
